445038a40b49a947fe10635c8db537b529c2bbe8
anhhv <anhhv@gotech.vn>
2023-03-03 12:06:27 +0700

    UI/UX

---
+++ app/src/main/java/anhhv/blood_alcohol_content/SplashActivity.java
@@ -0,0 +1,31 @@
+package anhhv.blood_alcohol_content;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        gotoHome();
+    }
+
+    private void gotoHome() {
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, HomeActivity.class));
+                finish();
+            }
+        }, 2000);
+    }
+
+}
