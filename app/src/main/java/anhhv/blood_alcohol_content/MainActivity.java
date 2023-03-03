47193a7f7872d4a857034f03bfc6f62544e1b5a7
anhhv <anhhv@gotech.vn>
2023-03-03 11:14:54 +0700

    Init

---
+++ app/src/main/java/anhhv/blood_alcohol_content/MainActivity.java
@@ -0,0 +1,14 @@
+package anhhv.blood_alcohol_content;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
